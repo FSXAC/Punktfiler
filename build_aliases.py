0791533afaaa40814c6e0720b18dada63efe8661
Muchen He <i@muchen.ca>
2020-08-24 17:35:33 -0700

    Finished build aliases script

--- build_aliases.py
+++ build_aliases.py
@@ -22,10 +22,36 @@ while sh_sel not in alias_format:
     if sh_sel.lower() == 'q':
         sys.exit(0)
 
+# Ask the user which aliases to include
+aliases_stage = dict()
+for idx, aliases_set in enumerate(aliases_list):
+    # print('\n\n\n')
+    print(f'SET {idx + 1} out of {len(aliases_list)}')
+    print(f'Do you wish to include: {aliases_set["__desc__"]}?')
+    
+    print()
+    for alias in aliases_set:
+        if not alias.startswith('__'):
+            print(' ‣  {:12}: {}'.format(alias, aliases_set[alias]))
+    print()
+
+    if input('y/n: ') == 'y':
+        aliases_stage.update(aliases_set)
+    
+    print('\n\n')
+
 # Make output directory
+print('Ouputing to \'output/\' folder . . .')
 if not os.path.exists('output'):
     os.makedirs('output')
 
 # Generate file
-out_fname = f'.{sh_sel}_aliases'
+print(f'Generating file for {sh_sel} . . .')
+out_fname = f'output/.{sh_sel}_aliases'
+with open(out_fname, 'w', encoding='utf-8') as outfile:
+    for alias in aliases_stage:
+        if not alias.startswith('__'):
+            outfile.write(alias_format[sh_sel].format(alias, aliases_stage[alias]))
+            outfile.write('\n')
 
+print('Done!')
